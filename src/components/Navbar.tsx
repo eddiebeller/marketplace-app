a078a92a8d18e47f74f7d81acabc1b89df097460
Eddie Beller <ieduard.beller@gmail.com>
2023-04-13 22:04:44 +0200

    :sparkles: display the messages in the offers page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,20 +41,31 @@ export const Navbar = () => {
                 className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
                 aria-current="page"
               >
-                Home
+                Browse Offers
               </Link>
             </li>
             {user.isSignedIn && (
-              <li>
-                <Link
-                  // TODO: change the name of the link
-                  href="/sell-an-item"
-                  className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Sell
-                </Link>
-              </li>
+              <>
+                <li>
+                  <Link
+                    href="/offers"
+                    className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    Offers
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    // TODO: change the name of the link
+                    href="/sell-an-item"
+                    className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                    aria-current="page"
+                  >
+                    Sell
+                  </Link>
+                </li>
+              </>
             )}
 
             {!user.isSignedIn && (
--- src/server/api/routers/item.ts
+++ src/server/api/routers/item.ts
@@ -23,13 +23,25 @@ export const itemRouter = createTRPCRouter({
         },
       });
     }),
+  getMessage: protectedProcedure.query(async ({ ctx }) => {
+    const items = await ctx.prisma.item.findMany({
+      where: {
+        userId: ctx.auth.userId as string,
+      },
+      include: {
+        message: true,
+      },
+    });
+    return items.flatMap((item) => item.message);
+  }),
   sendMessage: protectedProcedure
     .input(z.object({ message: z.string(), itemId: z.string() }))
     .mutation(async ({ input, ctx }) => {
       const message = await ctx.prisma.message.create({
         data: {
           fromUser: ctx.auth.userId as string,
-          fromUserName: ctx.auth.user?.username ?? "unknown",
+          // TODO: fix bug with the username - now ctx.auth.user is undefined
+          fromUserName: ctx.auth.user?.username as string,
           itemId: input.itemId,
           message: input.message,
         },
